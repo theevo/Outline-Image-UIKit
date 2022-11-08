13d80f1c9537a050229a16c0bb73c6cba70acff8
Theo Vora <theo.vora@gmail.com>
2022-11-08 12:02:47 -0500

    apply a typical border

--- Outline_Image_UIKit/ViewController.swift
+++ Outline_Image_UIKit/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fruitImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        fruitImageView.layer.borderWidth = 7
+        fruitImageView.layer.borderColor = UIColor.systemPink.cgColor
     }
 
 
